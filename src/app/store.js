cfb646b9df17064f174dab6b889446eeffbb93ef
Temitope Ogunleye <topeogunleye1@gmail.com>
2022-02-12 21:39:50 +0100

    Add usersReducer, postsReducer, and commentsReducer to the store

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
+import usersReducer from '../features/users/usersSlice';
+import postsReducer from '../features/posts/postsSlice';
+import commentsReducer from '../features/comments/commentsSlice';
 import counterReducer from '../features/counter/counterSlice';
 
 export const store = configureStore({
   reducer: {
+    users: usersReducer,
+    posts: postsReducer,
+    comments: commentsReducer,
     counter: counterReducer,
   },
 });
