80c9307c03fbe002bc0d0f7aea81c2cd1208ed6a
Temitope Ogunleye <topeogunleye1@gmail.com>
2022-02-14 21:20:54 +0100

    fetchData for bitcoin

--- src/features/counter/Counter.js
+++ src/features/counter/Counter.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import {
   decrement,
@@ -7,10 +7,14 @@ import {
   incrementAsync,
   incrementIfOdd,
   selectCount,
+  fetchData,
 } from './counterSlice';
 import styles from './Counter.module.css';
 
 export function Counter() {
+  useEffect(() => {
+    fetchData("bitcoin")
+  }, []);
   const count = useSelector(selectCount);
   const dispatch = useDispatch();
   const [incrementAmount, setIncrementAmount] = useState('2');
