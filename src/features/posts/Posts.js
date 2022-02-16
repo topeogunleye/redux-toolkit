e3e3a9e8b912a973308ff858ce4702a48945892f
Temitope Ogunleye <topeogunleye1@gmail.com>
2022-02-16 21:40:41 +0100

    Add Posts import code

---
+++ src/features/posts/Posts.js
@@ -0,0 +1,9 @@
+import React, { useEffect, useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { selectPosts, deletePost } from './postsSlice';
+
+function Posts() {
+  
+}
+
+export default Posts
